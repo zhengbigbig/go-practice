037634e66dcc33735edc04193b9896593cf1808b
zhengbigbig <780357902@qq.com>
2021-07-27 18:21:02 +0800

    变量声明

---
+++ src/github.com/01/var/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// 声明变量
+//var name string
+//var age int
+//var isOk bool
+// Go语言中推荐使用驼峰式命名
+// 批量声明
+var (
+	name string
+	age  int
+	isOk bool
+)
+
+func main() {
+	name = "lixiang"
+	age = 16
+	isOk = true
+	// Go语言中变量声明必须使用
+	fmt.Print(isOk) // 在终端中输入要打印的内容
+	fmt.Printf("name:%s", name) // %s 占位符，使用name变量的值去替换
+	fmt.Println(age) // 打印完指定的内容之后会在后面加一个换行符
+}
