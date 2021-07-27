dba04d2ed5c67ea926ca546e2ee71583d9fcccc8
zhengbigbig <780357902@qq.com>
2021-07-27 18:27:58 +0800

    类型推导、简短变量声明

--- src/github.com/01/var/main.go
+++ src/github.com/01/var/main.go
@@ -22,4 +22,14 @@ func main() {
 	fmt.Print(isOk) // 在终端中输入要打印的内容
 	fmt.Printf("name:%s", name) // %s 占位符，使用name变量的值去替换
 	fmt.Println(age) // 打印完指定的内容之后会在后面加一个换行符
+
+	// 声明变量同时赋值
+	var s1 string = "yyds"
+	fmt.Println(s1)
+	// 类型推导（根据值判断该变量是什么类型）
+	var s2 = "20"
+	fmt.Println(s2)
+	// 简短变量声明，只能在函数里面使用
+	s3 := "hahaha"
+	fmt.Println(s3)
 }
