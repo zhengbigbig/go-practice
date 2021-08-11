dc8ea9fb3d3aad0b96a76e8c6e959417b29ea83d
zhengbigbig <780357902@qq.com>
2021-08-11 18:59:02 +0800

    使用Notify来通知

---
+++ src/github.com/12-并发/异步日志/customLogger/console.go
@@ -0,0 +1,51 @@
+package customLogger
+
+import (
+	"fmt"
+	"time"
+)
+
+// Logger 往终端写日志相关内容
+// Logger日志结构体
+type ConsoleLogger struct {
+	Level LogLevel
+}
+
+func NewConsoleLog(levelStr string) ConsoleLogger {
+	level, err := parseLogLevel(levelStr)
+	if err != nil {
+		panic(err)
+	}
+	return ConsoleLogger{
+		Level: level,
+	}
+}
+
+func (c ConsoleLogger) enable(logLevel LogLevel) bool {
+	return c.Level <= logLevel
+}
+
+func (c ConsoleLogger) log(lv LogLevel, format string, a ...interface{}) {
+	if c.enable(lv) {
+		msg := fmt.Sprintf(format, a...)
+		now := time.Now()
+		funcName, fileName, lineNo := getInfo(3) // 从里到外
+		fmt.Printf("[%s] [%s] [%s:%s:%d] %s\n", now.Format("2006-01-02 15:04:05"), getLogString(lv), fileName, funcName, lineNo, msg)
+	}
+}
+
+func (c ConsoleLogger) Debug(format string, a ...interface{}) {
+	c.log(DEBUG, format, a...)
+}
+func (c ConsoleLogger) Info(format string, a ...interface{}) {
+	c.log(INFO, format, a...)
+}
+func (c ConsoleLogger) Warning(format string, a ...interface{}) {
+	c.log(WARNING, format, a...)
+}
+func (c ConsoleLogger) Error(format string, a ...interface{}) {
+	c.log(ERROR, format, a...)
+}
+func (c ConsoleLogger) Fatal(format string, a ...interface{}) {
+	c.log(FATAL, format, a...)
+}
